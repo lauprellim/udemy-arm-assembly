9cd2b7f9aadb390c3c5bd43826443a2add3b0d30
Paul V. Miller <86619288+lauprellim@users.noreply.github.com>
2025-06-11 12:37:50 -0400

    Delete unit-15-bare-metal-programming directory

--- unit-15-bare-metal-programming/build.sh
+++
@@ -1,4 +0,0 @@
-aarch64-linux-gnu-gcc -c boot.s -o boot.o
-aarch64-linux-gnu-gcc -c main.s -o main.o
-aarch64-linux-gnu-ld -nostdlib -T link.lds -o kernel boot.o main.o
-aarch64-linux-gnu-objcopy -O binary kernel kernel8.img
